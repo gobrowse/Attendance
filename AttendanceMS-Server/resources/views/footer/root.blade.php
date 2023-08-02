45a274d2ef4eb0d30b205541b529f464a5d5d1ed
Gobrowse <67398321+gobrowse@users.noreply.github.com>
2023-08-02 15:52:46 -0700

    Update root.blade.php

--- AttendanceMS-Server/resources/views/footer/root.blade.php
+++ AttendanceMS-Server/resources/views/footer/root.blade.php
@@ -2,11 +2,6 @@
     <div class="container">
         <div class="row">
             <div class="col l6 s12">
-                <h5 class="white-text">Company Bio</h5>
-                <p class="grey-text text-lighten-4">We are a team of college students working on this project like
-                    it's our full time job. Any amount would help support
-                    and continue development on this project and is greatly appreciated.
-                </p>
             </div>
             <div class="col l3 s12">
                 <h5 class="white-text">Core</h5>
@@ -53,10 +48,6 @@
                 <h5 class="white-text">Supporting</h5>
                 <ul>
                     <li>
-                        <div class="chip">
-                            <img src="{{ asset("/img/ajax_logo.png") }}">
-                            Ajax
-                        </div>
                     </li>
                     <li>
                         <div class="chip">
